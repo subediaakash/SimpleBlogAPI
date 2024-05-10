1c924ab71166eb2d506eac0a1f67fa8d9fc0ce4a
spartan-readytocode <7007966033abc@gmail.com>
2024-05-10 22:12:19 +0530

    feat : create Post section updated

--- frontend/src/pages/CreatePost.tsx
+++ frontend/src/pages/CreatePost.tsx
@@ -1,9 +1,15 @@
 import Addpost from "../components/Addpost";
+import Navbar from "../components/Navbar";
 
 const CreatePost = () => {
   return (
-    <div>
-      <Addpost />
+    <div className="bg-[#17252e] h-[100vh]">
+      <div className="bg-white">
+        <Navbar />
+      </div>
+      <div className="flex justify-center h-[100%] items-center">
+        <Addpost />
+      </div>
     </div>
   );
 };
