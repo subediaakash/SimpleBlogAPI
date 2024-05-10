801214f10829f0d7fd121bf72ee932ce65b39686
spartan-readytocode <7007966033abc@gmail.com>
2024-05-10 22:13:50 +0530

    feat : updated the get user's posts

--- frontend/src/components/GetPost.tsx
+++ frontend/src/components/GetPost.tsx
@@ -1,14 +1,24 @@
 import React from "react";
+import { FaRegEdit } from "react-icons/fa";
 
 const GetPost = ({ props }: any) => {
   return (
-    <div>
+    <div
+      className="flex justify-center flex-col items-center gap-4
+    "
+    >
       {props.map((prop: any) => {
         return (
-          <React.Fragment key={prop.id}>
-            <h2>{prop.title}</h2>
-            <h3>{prop.content}</h3>
-          </React.Fragment>
+          <div className="w-1/3 px-5 flex flex-col gap-2">
+            <React.Fragment key={prop.id}>
+              <div className="flex justify-between items-center">
+                {" "}
+                <h2 className="text-xl font-bold">{prop.title}</h2>
+                <FaRegEdit />
+              </div>
+              <h3 className="text-slate-500">{prop.content}</h3>
+            </React.Fragment>
+          </div>
         );
       })}
     </div>
